b577aae1f1877876ae715eaa4705f269386ceacf
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-05 22:44:39 +0900

    함수로 변경

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}//maxDaysOfMonth
+
 	public static void main(String[] args) {
 
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 
 		System.out.println("달을 입력하세요");
 
 		int month = scanner.nextInt();
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-		System.out.printf("%d월은 %d일까지 있습니다/n", month, maxDays[month - 1]);
+		System.out.printf("%d월은 %d일까지 있습니다/n", month, cal.maxDaysOfMonth(month));
 
 		scanner.close();
 
 	}//main
 
+	
 }//Calendar
