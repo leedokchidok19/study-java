2b5b4eda7788ecba250860c41dc768b786e0c426
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-05 22:53:37 +0900

    for문을 사용해서 반복횟수 기능 추가

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -16,11 +16,20 @@ public class Calendar {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.println("달을 입력하세요");
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
 
-		int month = scanner.nextInt();
+		for(int i = 0;i < repeat; i++) {
 
-		System.out.printf("%d월은 %d일까지 있습니다/n", month, cal.maxDaysOfMonth(month));
+			System.out.println("달을 입력하세요");
+
+			int month = scanner.nextInt();
+
+			System.out.printf("%d월은 %d일까지 있습니다/n", month, cal.maxDaysOfMonth(month));
+
+		}
+
+		System.out.println("종료되었습니다");
 
 		scanner.close();
 
