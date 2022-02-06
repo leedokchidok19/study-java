d01a9ea2e60441bca106fe1582a44ec2d312cd2b
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-06 21:51:44 +0900

    1.종료 조건을 이용해서 반복하기 2.오타 수정

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -13,19 +13,29 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		String PROMPT = "calendar> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
+		System.out.println("반복횟수를 입력하세요");
 
-		for(int i = 0;i < repeat; i++) {
+		int month = 1;
+
+		while(true) {
 
 			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
 
-			int month = scanner.nextInt();
+			month = scanner.nextInt();
+			
+			if(month  < 1) break;
+			
+			if(month >= MAX_DAYS.length) {
+				System.out.println("1~12월 사이로 입력해 주세요");
+				continue;
+			}
 
-			System.out.printf("%d월은 %d일까지 있습니다/n", month, cal.maxDaysOfMonth(month));
+			System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.maxDaysOfMonth(month));
 
 		}
 
