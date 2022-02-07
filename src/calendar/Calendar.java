2a7b87f37cd083d01f63dc9b3c02ac63e19a3af6
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-07 23:53:26 +0900

    입력한 월에 맞는 가상의 달력 찍기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -6,44 +6,36 @@ public class Calendar {
 
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	public int maxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}//maxDaysOfMonth
 
-	public static void main(String[] args) {
+	/*
+	 * - 월을 입력하면 해당월의 달력을 출력한다.
+	 * - 달력은 모양은 1단계에서 작성한 모양으로 만든다.
+	 * - 1일은 일요일로 정해도 무관하다.
+	 * - -1을 입력받기 전까지 반복 입력받는다.
+	 * - 프롬프트를 출력한다.
+	 * */
 
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		String PROMPT = "calendar> ";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
+	public void printCalendar(int year, int month) {
 
-		System.out.println("반복횟수를 입력하세요");
+		// %4d 4칸, %3d 3칸 의미
+		System.out.printf("  <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
 
-		int month = 1;
+		int maxDay = getMaxDaysOfMonth(month);
 
-		while(true) {
+		for(int i = 1; i <= maxDay; i++) {
 
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.printf("%3d", i);
+			if(i % 7 == 0) System.out.println();
 
-			month = scanner.nextInt();
-			
-			if(month  < 1) break;
-			
-			if(month >= MAX_DAYS.length) {
-				System.out.println("1~12월 사이로 입력해 주세요");
-				continue;
-			}
+		}//for
 
-			System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.maxDaysOfMonth(month));
+		System.out.println();
 
-		}
+	}//printSampleCalendar
 
-		System.out.println("종료되었습니다");
-
-		scanner.close();
-
-	}//main
-
-	
 }//Calendar
---
+++ src/calendar/Prompt.java
@@ -0,0 +1,52 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+
+	public final static String PROMPT = "calendar> ";
+
+	public void runPrompt() {
+
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		System.out.println("반복횟수를 입력하세요");
+
+		int month = 1;
+
+		while(true) {
+
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+
+			month = scanner.nextInt();
+					
+			if(month < 1) break;
+					
+			if(month > 12) {
+				System.out.println("1~12월 사이로 입력해 주세요");
+				continue;
+			}
+
+			cal.printCalendar(2022, month);
+
+		}//while
+
+		System.out.println("종료되었습니다");
+
+		scanner.close();
+
+	}//runPrompt
+
+	public static void main(String[] args) {
+
+		// 셀 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}//main
+
+}//Prompt
