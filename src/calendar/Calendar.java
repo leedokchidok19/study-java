9d10b498b6c2f8d1cde3be6d9da8b5719395deae
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-09 22:46:46 +0900

    요일을 입력해서 정교한 달력찍기 1. 요일 추가 2. 종료 조건 변경

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -30,23 +30,47 @@ public class Calendar {
 	 * - 프롬프트를 출력한다.
 	 * */
 
-	public void printCalendar(int year, int month) {
-
+	public void printCalendar(int year, int month, int weekday) {
+System.out.println(weekday);
 		// %4d 4칸, %3d 3칸 의미
 		System.out.printf("  <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
+		//print blank space
+		for(int i =0; i < weekday; i++) {
+			System.out.printf("   ");
+		}
+
 		int maxDay= getMaxDaysOfMonth(year, month);
- 
-		for(int i = 1; i <= maxDay; i++) {
+		int count = 7 - weekday;
+		int delim = ( count < 7 ) ? count : 0;
+		/* int delim = ( count < 7 ) ? count : 0;
+		 * int delim;
+		 * if(count < 7) {
+		 * 	delim = count;
+		 * } else {
+		 * 	delim = 0;
+		 * }
+		 * */
+
+		//print first line
+		for(int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+
+		System.out.println();
+
+		//print from second line to last
+		count++;//다음 줄도 동일한 숫자가 나오기 때문에 더하기 1을 해준다
+		for(int i = count; i <= maxDay; i++) {
 
 			System.out.printf("%3d", i);
-			if(i % 7 == 0) System.out.println();
+			if(i % 7 == delim) System.out.println();
 
 		}//for
 
-		System.out.println();
+		System.out.println("\n");
 
 	}//printSampleCalendar
 
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class Prompt {
 
 
-	public final static String PROMPT = "calendar> ";
+	/*
+	 * @param week 요일명
+	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+	 * */
+	public int parseDay(String week) {
+		switch (week) {
+		case "su": return 0;
+		case "mo": return 1;
+		case "tu": return 2;
+		case "we": return 3;
+		case "th": return 4;
+		case "fr": return 5;
+		case "sa": return 6;
+		default: return 0;
+			//throw new IllegalArgumentException("Unexpected value: " + week);
+		}
+	}
 
 	public void runPrompt() {
 
@@ -17,24 +33,32 @@ public class Prompt {
 
 		int month = 1;
 		int year = -1;
+		int weekday = 0;
 
 		while(true) {
 
-			System.out.println("년도를 입력하세요");
+			System.out.println("년도를 입력하세요 (exit: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+
+			//종료
+			if(year == -1) break;
+
 			System.out.println("달을 입력하세요");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-					
-			if(month < 1) break;
-					
-			if(month > 12) {
+
+			//잘못된 입력
+			if(month > 12 || month < 1) {
 				System.out.println("1~12월 사이로 입력해 주세요");
 				continue;
 			}
 
-			cal.printCalendar(year, month);
+			System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, we, th, fr, sa).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+			cal.printCalendar(year, month, weekday);
 
 		}//while
 
